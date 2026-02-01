fe00a5cade26c5650c0c088d88bb5573045ecbce
pandagg110 <wudiqing@hotmail.com>
2026-02-01 17:26:06 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-4xl">
-        百业战 终端 - 潘荙荙最帅 凤鸣藏第二帅版
+        百业战 终端 - 潘荙荙最帅 凤鸣藏和🍟第二帅
       </div>
       <div className="mt-4 text-neutral-400">
         系统初始化中...
